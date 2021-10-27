72ca204b6275c9978a03ffe02f06070f73440acf
riskin88 <hlavac.honza@email.cz>
2021-10-27 17:30:55 +0200

    Initial commit, files generated by Spring initializr

---
+++ server/src/main/java/cz/cvut/fit/tjv/hlavaj39/semestral/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.tjv.hlavaj39.semestral.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
